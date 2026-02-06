b270c234b16771e287f9ce476e35790aba8d03b0
rbcodewalker <bastakotirajdeep60@gmail.com>
2026-02-06 14:10:05 +0100

    Initial Commit

---
+++ Avatar Lab/Avatar_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avatar_LabApp.swift
+//  Avatar Lab
+//
+//  Created by rbastako on 06.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Avatar_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
